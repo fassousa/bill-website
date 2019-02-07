d7e2aec4837bb2fe9830f90b16a2c22df8fe555a
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2019-02-07 13:00:57 -0200

    Add Semantic Ui React to project

--- src/components/BillsContainer.js
+++ src/components/BillsContainer.js
@@ -11,15 +11,28 @@ class App extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:3001/api/v1/bills.json")
+      .get("https://floating-journey-70389.herokuapp.com/api/v1/bills.json")
       .then(response => {
         console.log(response);
         this.setState({ bills: response.data });
       })
       .catch(error => console.log(error));
   }
+
   render() {
-    return <div className="">Bills</div>;
+    return (
+      <div>
+        {console.log(this.state.bills[0])}
+        {this.state.bills.map(bill => {
+          return (
+            <div className="tile" key={bill.id}>
+              <h4>{bill.author}</h4>
+              <p>{bill.id}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
