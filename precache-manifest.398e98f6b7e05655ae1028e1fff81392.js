527a7fb142344981ce7d91b27be33d7270246377
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2019-02-08 17:41:59 -0200

    Updates

---
+++ precache-manifest.398e98f6b7e05655ae1028e1fff81392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cd85c8fbe2a853db48d",
+    "url": "/bill-website/static/js/runtime~main.7cd85c8f.js"
+  },
+  {
+    "revision": "bb2be1fc189eef7f94c3",
+    "url": "/bill-website/static/js/main.bb2be1fc.chunk.js"
+  },
+  {
+    "revision": "6531a418df38752a2d10",
+    "url": "/bill-website/static/js/1.6531a418.chunk.js"
+  },
+  {
+    "revision": "bb2be1fc189eef7f94c3",
+    "url": "/bill-website/static/css/main.e69fe4fc.chunk.css"
+  },
+  {
+    "revision": "8f8b82523c4e70ce0320918202236ae5",
+    "url": "/bill-website/index.html"
+  }
+];
